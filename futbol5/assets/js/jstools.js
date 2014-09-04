2394530c18b5cfd59ad66b5fdb645267757474e5
danielbellido <dbellidovasquez@gmail.com>
2014-09-04 04:09:18 -0500

    cambios 3.0
    el datetimepicker incluido, aunque posiblemente se cambie por otro
    datetimepicker (mismo nombre) debido a que habia otro que solo podía
    ingresar una hora en punto (se acomoda más al proyecto)

--- futbol5/assets/js/jstools.js
+++ futbol5/assets/js/jstools.js
@@ -6,3 +6,18 @@ return true;
  
 return /\d/.test(String.fromCharCode(keynum));
 }
+var listaEstandar = [];
+document.getElementById('usuarioEstandar').innerHTML = listaEstandar;
+var listaSolidaria = [];
+
+function agregarEstandar(e)
+{
+	
+	listaEstandar.push(document.getElementById('usuarioEstandar'));
+	document.getElementById('usuarioEstandar').innerHTML = listaEstandar;
+
+}
+/*funciones para la parte de las reservas*/
+$(function () {
+                $('#datetimepicker1').datetimepicker();
+            });
