28967994a35c32a7bcb14957bb58ec643754b6d4
danielbellido <dbellidovasquez@gmail.com>
2014-09-04 12:38:54 -0500

    cambios 3.1
    se cambió de plugin para elegir la hora y el día de la reserva por uno
    más sencillo (?) de usar

--- futbol5/assets/js/jstools.js
+++ futbol5/assets/js/jstools.js
@@ -19,5 +19,27 @@ function agregarEstandar(e)
 }
 /*funciones para la parte de las reservas*/
 $(function () {
-                $('#datetimepicker1').datetimepicker();
-            });
+	$('.form_date').datetimepicker({
+        language:  'es',
+        weekStart: 1,
+        todayBtn:  1,
+		autoclose: 1,
+		todayHighlight: 1,
+		startView: 2,
+		minView: 2,
+		forceParse: 0,
+		startDate: '2014-09-04'
+    });
+	$('.form_time').datetimepicker({
+        language:  'es',
+        weekStart: 1,
+        todayBtn:  1,
+		autoclose: 1,
+		todayHighlight: 1,
+		startView: 1,
+		minView: 0,
+		maxView: 1,
+		forceParse: 0,
+		minuteStep: 60
+    });            
+});
